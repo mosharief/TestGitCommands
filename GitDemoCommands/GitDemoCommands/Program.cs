55fa15317dbb4d6dc1d7547cdcb32be8e7fdfe16
Mohammed Sharief <mosharief@gmail.com>
2019-08-26 11:33:27 -0400

    First Git Command Demo

---
+++ GitDemoCommands/GitDemoCommands/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoCommands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Demo Commands");
+            Console.ReadLine();
+        }
+    }
+}
